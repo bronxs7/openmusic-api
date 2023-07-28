899c51ee30680f0bf0b427b0c56a9339dccba46a
bronxs7 <arisman7@gmail.com>
2023-07-28 17:01:07 +0700

    restructure api, add handler, routes, index & add album service in memory

--- src/server.js
+++ src/server.js
@@ -1,18 +1,29 @@
 const Hapi = require('@hapi/hapi');
+const albums = require('./api/music');
+const AlbumsService = require('./services/inMemory/AlbumsService');
 
 const init = async () => {
+  const albumsService = new AlbumsService();
+
   const server = Hapi.server({
-    port: 4000,
-    host: 'localhost',
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: albums,
+    options: {
+      service: albumsService,
+    },
   });
 
   await server.start();
   console.log('Server running on %s', server.info.uri);
 };
-
-process.on('unhandledRejection', (err) => {
-  console.log(err);
-  process.exit(1);
-});
 
 init();

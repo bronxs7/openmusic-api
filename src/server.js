73f0b1acba68a0bf1ac71621eb65323866e5bb83
bronxs7 <arisman7@gmail.com>
2023-07-28 17:25:25 +0700

    implement joi validator & installing dotenv

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const albums = require('./api/music');
 const AlbumsService = require('./services/inMemory/AlbumsService');
+const AlbumsValidator = require('./validator/albums');
 
 const init = async () => {
   const albumsService = new AlbumsService();
 
   const server = Hapi.server({
-    port: 5000,
+    port: process.env.PORT,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
@@ -19,6 +22,7 @@ const init = async () => {
     plugin: albums,
     options: {
       service: albumsService,
+      validator: AlbumsValidator,
     },
   });
 

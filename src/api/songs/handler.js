c35523900f6728afa290123532621376cde783eb
bronxs7 <arisman7@gmail.com>
2023-08-01 19:25:30 +0700

    build plugin songs & added into server.js

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -4,13 +4,13 @@ class SongsHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
-  }
 
-  this.postSongHandler = this.postSongHandler.bind(this);
-  this.getSongsHandler = this.getSongsHandler.bind(this);
-  this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
-  this.putSongByIdHandler = this.putSongByIdHandler.bind(this);
-  this.deleteSongByIdHandler = this.deleteSongByIdHandler.bind(this);
+    this.postSongHandler = this.postSongHandler.bind(this);
+    this.getSongsHandler = this.getSongsHandler.bind(this);
+    this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
+    this.putSongByIdHandler = this.putSongByIdHandler.bind(this);
+    this.deleteSongByIdHandler = this.deleteSongByIdHandler.bind(this);
+  }
 
   postSongHandler(request, h) {
     try {
@@ -37,6 +37,13 @@ class SongsHandler {
         response.code(error.statusCode);
         return response;
       }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Lagu gagal ditambahkan, ada kesalahan internal server',
+      });
+      response.code(500);
+      return response;
     }
   }
 
@@ -68,7 +75,14 @@ class SongsHandler {
         });
         response.code(error.statusCode);
         return response;
-      } 
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Gagal menemukan lagu, ada kesalahan internal server',
+      });
+      response.code(500);
+      return response;
     }
   }
 
@@ -92,6 +106,13 @@ class SongsHandler {
         response.code(error.statusCode);
         return response;
       }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Gagal mengupdate lagu, ada kesalahn internal server',
+      });
+      response.code(500);
+      return response;
     }
   }
 
@@ -110,9 +131,18 @@ class SongsHandler {
           status: 'fail',
           message: error.message,
         });
-        response.code(erro.statusCode);
+        response.code(error.statusCode);
         return response;
       }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Lagu gagal di hapus, ada kesalahan internal server',
+      });
+      response.code(500);
+      return response;
     }
   }
 }
+
+module.exports = SongsHandler;
